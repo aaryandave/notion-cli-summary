7e206be4f44be0bb8f5456cd9a5df0e657b707b4
Aaryan Dave <aaryan.r.dave@gmail.com>
2024-07-02 11:59:15 -0400

    Rewrite db client with notion-client package. include today view for tasks.

--- cli.py
+++ cli.py
@@ -2,7 +2,7 @@
 """
 import json
 import argparse
-from notion_client import NotionClient
+from notion_db_client import NotionClient
 from fuzzy_search import fuzzy_search
 
 def print_results(results: list[tuple]) -> None:
@@ -16,14 +16,27 @@ def print_results(results: list[tuple]) -> None:
     for result in results:
         print(f"- {result[0]} (relevance: {result[1]})")
 
+def print_today(tasks: list[str]) -> None:
+    """Print the tasks due today
+
+    Args:
+        tasks (list[str]): List of the items due today
+    """
+    print("Tasks to get done today:")
+    print("---------------")
+    for task in tasks:
+        print(f"- {task}")
+
 def main() -> None:
     """Main function to run the fuzzy search
     """
     parser = argparse.ArgumentParser(description="Search for items in the Notion database")
-    parser.add_argument("query", type=str, help="The query to search for")
+    # optional argument query, can be --query or -q
+    parser.add_argument("--query", "-q", type=str, help="Query to search for")
     parser.add_argument("--config", type=str, default=".config.json",
                         help="Path to the config file")
     parser.add_argument("--k", type=int, default=3, help="Number of results to return")
+    parser.add_argument("--today", action="store_true", help="Get the items due today")
 
     args = parser.parse_args()
 
@@ -31,9 +44,16 @@ def main() -> None:
         config_items = json.load(f)
 
     client = NotionClient(config_items["NOTION_API_KEY"], config_items["NOTION_DATABASE_ID"])
-    choices = client.get_page_strings()
-    search_results = fuzzy_search(args.query, choices, args.k)
-    print_results(search_results)
+
+    if args.query:
+        choices = client.get_page_strings(client.get_recent())
+        search_results = fuzzy_search(args.query, choices, args.k)
+        print_results(search_results)
+
+    if args.today:
+        tasks = client.get_today()
+        task_strings = client.get_page_strings(tasks)
+        print_today(task_strings)
 
 if __name__ == "__main__":
     main()
---
+++ notion_db_client.py
@@ -0,0 +1,146 @@
+"""Module to interact with Notion API."""
+from datetime import datetime, timedelta
+import logging
+from typing import List, Optional, Dict, Any
+import requests
+from notion_client import Client
+
+logging.basicConfig(level=logging.INFO)
+
+class NotionClient:
+    """Class to create a NotionClient object to interact with the Notion API."""
+
+    def __init__(self, notion_api_key: str, notion_database_id: str) -> None:
+        """
+        Constructor for the NotionClient class.
+
+        Args:
+            notion_api_key (str): API key for the Notion API
+            notion_database_id (str): ID of the database to interact with
+        """
+        self.api_key: str = notion_api_key
+        self.database_id: str = notion_database_id
+        self.headers: Dict[str, str] = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28"
+        }
+        self.database = Client(auth=self.api_key)
+
+    def get_name_from_id(self, page_id: str) -> Optional[str]:
+        """
+        Get the name of the page from the page ID.
+
+        Args:
+            page_id (str): ID of the page to retrieve
+
+        Returns:
+            Optional[str]: Name of the page or None if an error occurs
+        """
+        url = f"https://api.notion.com/v1/pages/{page_id}"
+        try:
+            response = requests.get(url, headers=self.headers, timeout=10)
+            response.raise_for_status()
+            json_data = response.json()
+            page_name = json_data["properties"]["Class"]["title"][0]["plain_text"]
+            return page_name
+        except requests.RequestException as e:
+            logging.error("Error fetching page name for ID {%s}: %s",page_id, e)
+            return None
+
+    def get_recent(self, num_items: int = 100) -> Optional[List[Dict[str, Any]]]:
+        """
+        Get the most recent items from the database.
+
+        Args:
+            num_items (int, optional): Number of items to retrieve. Defaults to 100.
+
+        Returns:
+            Optional[List[Dict[str, Any]]]: JSON data from the API response or None if 
+            an error occurs
+        """
+        try:
+            response = self.database.databases.query(
+                **{
+                    "database_id": self.database_id,
+                    "page_size": num_items,
+                    "filter": {
+                        "property": "Complete",
+                        "checkbox": {
+                            "equals": False
+                        }
+                    }
+                }
+            )
+            return response.get("results")
+        except Exception as e:
+            logging.error("Error fetching recent items: {%s}", e)
+            return None
+
+    def get_today(self) -> Optional[List[Dict[str, Any]]]:
+        """
+        Get the items due today or before.
+
+        Returns:
+            Optional[List[Dict[str, Any]]]: JSON data from the API response or None 
+            if an error occurs
+        """
+        today = datetime.now()
+        tomorrow = today + timedelta(days=1)
+        tomorrow_iso = tomorrow.date().isoformat()
+        try:
+            response = self.database.databases.query(
+                **{
+                    "database_id": self.database_id,
+                    "filter": {
+                        "and": [
+                            {
+                                "property": "Complete",
+                                "checkbox": {
+                                    "equals": False
+                                }
+                            },
+                            {
+                                "property": "Date",
+                                "date": {
+                                    "before": tomorrow_iso
+                                }
+                            }
+                        ]
+                    }
+                }
+            )
+            return response.get("results")
+        except Exception as e:
+            logging.error("Error fetching today's items: {%s}", e)
+            return None
+
+    def get_page_strings(self, json_data: Optional[List[Dict[str, Any]]]) -> List[str]:
+        """
+        Get the page names from the JSON data.
+
+        Args:
+            json_data (Optional[List[Dict[str, Any]]]): JSON data from the API response
+
+        Returns:
+            List[str]: List of the page names
+        """
+        if not json_data:
+            return []
+
+        page_strings = []
+        for item in json_data:
+            try:
+                page_name = item["properties"]["Name"]["title"][0]["plain_text"]
+                page_date = item["properties"]["Date"]["date"]["start"]
+                page_type = item["properties"]["Type"]["select"]["name"]
+                class_id = item["properties"]["Class"]["relation"][0]["id"]
+                class_name = self.get_name_from_id(class_id)
+                if class_name:
+                    page_string = f"{page_name}, a {page_type} for {class_name} due on {page_date}"
+                    page_strings.append(page_string)
+            except KeyError as e:
+                logging.error("Error processing item: {%s}", e)
+                continue
+
+        return page_strings
